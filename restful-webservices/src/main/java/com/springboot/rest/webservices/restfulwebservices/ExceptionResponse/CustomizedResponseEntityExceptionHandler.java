04d8d5615a156ed3791c829b067a43af4dc3c873
Gautham D N <gauthamdn@gmail.com>
2020-08-07 22:14:04 +0530

    Adding Validations on Rest services
    Adding Validations on Rest services

--- restful-webservices/src/main/java/com/springboot/rest/webservices/restfulwebservices/ExceptionResponse/CustomizedResponseEntityExceptionHandler.java
+++ restful-webservices/src/main/java/com/springboot/rest/webservices/restfulwebservices/ExceptionResponse/CustomizedResponseEntityExceptionHandler.java
@@ -2,8 +2,10 @@ package com.springboot.rest.webservices.restfulwebservices.ExceptionResponse;
 
 import java.util.Date;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,4 +44,18 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 		
 	}
 
+	// to handle validation failures - overide the below method from ResponseEntityExceptioHandler
+	@Override	
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+		// store the binding result which contains the validation error as description to the custom exceptionresponse object
+		ExceptionResponse exceptionResponse =
+				new ExceptionResponse(new Date(),"Validation Failed",
+						ex.getBindingResult().toString());
+		
+		return new ResponseEntity(exceptionResponse,HttpStatus.BAD_REQUEST);
+	}
+
+	
 }
