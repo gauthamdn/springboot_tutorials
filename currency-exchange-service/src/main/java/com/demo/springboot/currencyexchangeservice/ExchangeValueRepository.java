01f9ea8b0918de05d6543a8db03565ae61c7ac52
Gautham D N <gauthamdn@gmail.com>
2020-08-31 20:09:10 +0530

    spring boot tutorials
    spring boot tutorials

---
+++ currency-exchange-service/src/main/java/com/demo/springboot/currencyexchangeservice/ExchangeValueRepository.java
@@ -0,0 +1,10 @@
+package com.demo.springboot.currencyexchangeservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeValueRepository extends JpaRepository<ExchangeValue,Long>{
+
+	
+	ExchangeValue findByFromAndTo(String from, String to);
+	
+}
--- limits-service/limits-service/src/main/java/com/demo/microservices/limitsservice/LimitConfigurationController.java
+++ limits-service/limits-service/src/main/java/com/demo/microservices/limitsservice/LimitConfigurationController.java
@@ -1,6 +1,8 @@
 package com.demo.microservices.limitsservice;
 
 import com.demo.microservices.limitsservice.bean.LimitConfiguration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +16,16 @@ public class LimitConfigurationController {
     @GetMapping("/limits")
     public LimitConfiguration retrieveLimitsFromConfiguration(){
         return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+    }
+    
+    @GetMapping("/fault-tolerance-demo")
+    @HystrixCommand(fallbackMethod = "fallbackRetrieveConfigruation")
+    public LimitConfiguration retriveLimits() {
+    	throw new RuntimeException("Service Not available");
+    }
+    
+    public LimitConfiguration fallbackRetrieveConfigruation() {
+    	return new LimitConfiguration(999,9);
     }
 
 }
